bcce3b13a6d83f58ea7d45be55e9c947d236dae8
Léopold Guyot <leopold.guyot13@gmail.com>
2022-11-16 09:42:10 +0100

    Update source_code.R

--- source_code.R
+++ source_code.R
@@ -3,83 +3,27 @@ library(seqinr)
 source("seqs.R")
 
 #function :
-alignement <- function(title,code, name){
-  seqs <- read.GenBank(code, 
-                       name,
+alignement <- function(title){
+  codes <- eval(as.symbol(title))
+  names <- eval(as.symbol(paste(title, "_names", sep = "")))
+  seqs <- read.GenBank(codes, 
+                       names,
                        as.character = T)
-  names(seqs) <- name
-  
-  write.fasta(seqs, name,
-              file="data_output/" + title + ".fasta")
+  names(seqs) <- names
+  file_output = paste("data_output/", title, ".fasta", sep = "")
+  write.fasta(seqs, names, file=file_output)
   
   #Alignement
+  file_output_align = paste("data_output/", title, "_align.fasta", sep = "") 
   dnabin_seqs <- as.DNAbin(seqs)
   muscle(dnabin_seqs, exec = "muscle",
          MoreArgs = "", quiet = TRUE,
-         file = "data_output/" + title + "_align.fasta")
+         file = file_output_align)
 }
 
-alignement("Cassidinae_28S_D28test", Cassidinae_28S_D2, Cassidinae_28S_D2_names)
-# Cassidinae (hôtes) :
-######################Cassidinae_28S_D2#########################
-seqs_Cassidinae_28S_D2 <- read.GenBank(Cassidinae_28S_D2, 
-                                       Cassidinae_28S_D2_names,
-                                       as.character = T)
-names(seqs_Cassidinae_28S_D2) <- Cassidinae_28S_D2_names
+list <- c("Cassidinae_28S_D2", "Eulo_28S_D2", "Eulo_ITS2", "Eulo_Cyt_b")
 
-write.fasta(seqs_Cassidinae_28S_D2, Cassidinae_28S_D2_names,
-            file="data_output/Cassidinae_28S_D2.fasta")
+for (x in list){
+  alignement(x)
+}
 
-#Alignement
-dnabin_Cassidinae_28S_D2 <- as.DNAbin(seqs_Cassidinae_28S_D2)
-muscle(dnabin_Cassidinae_28S_D2, exec = "muscle",
-       MoreArgs = "", quiet = TRUE,
-       file = "data_output/Cassidinae_28S_D2_align.fasta")
-
-
-# Eulophidae (parasitoïdes)
-####################### 28S D2 #########################
-seqs_Eulo_28S_D2 <- read.GenBank(Eulo_28S_D2,
-                                 Eulo_28S_D2_names,
-                                 as.character = T)
-names(seqs_Eulo_28S_D2) <- Eulo_28S_D2_names
-
-write.fasta(seqs_Eulo_28S_D2, Eulo_28S_D2_names,
-            file="data_output/Eulo_28S_D2.fasta")
-
-#alignement
-dnabin_Eulo_28S_D2 <- as.DNAbin(seqs_Eulo_28S_D2)
-muscle(dnabin_Eulo_28S_D2, exec = "muscle",
-       MoreArgs = "", quiet = TRUE,
-       file = "data_output/Eulo_28S_D2_align.fasta")
-
-###################### ITS2 ##########################
-seqs_Eulo_ITS2 <- read.GenBank(Eulo_ITS2, 
-                               Eulo_ITS2_names,
-                                       as.character = T)
-names(seqs_Eulo_ITS2) <- Eulo_ITS2_names
-
-write.fasta(seqs_Eulo_ITS2, Eulo_ITS2_names,
-            file="data_output/Eulo_ITS2.fasta")
-
-#alignement
-dnabin_Eulo_ITS2 <- as.DNAbin(seqs_Eulo_ITS2)
-muscle(dnabin_Eulo_ITS2, exec = "muscle",
-       MoreArgs = "", quiet = TRUE,
-       file = "data_output/Eulo_ITS2_align.fasta")
-
-
-##################### Cytochrome b ####################
-seqs_Eulo_Cyt_b <- read.GenBank(Eulo_Cyt_b, 
-                                Eulo_Cyt_b_names,
-                                as.character = T)
-names(seqs_Eulo_Cyt_b) <- Eulo_Cyt_b_names
-
-write.fasta(seqs_Eulo_Cyt_b, Eulo_Cyt_b_names,
-            file="data_output/Eulo_Cyt_b.fasta")
-
-#alignement
-dnabin_Eulo_Cyt_b <- as.DNAbin(seqs_Eulo_Cyt_b)
-muscle(dnabin_Eulo_Cyt_b, exec = "muscle",
-       MoreArgs = "", quiet = TRUE,
-       file = "data_output/Eulo_Cyt_b_align.fasta")
