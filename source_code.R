94ca778cbfbe955cce87a39c69ce6b5e9097f0c6
Léopold Guyot <leopold.guyot13@gmail.com>
2022-12-14 14:06:45 +0100

    trees

--- source_code.R
+++ source_code.R
@@ -31,11 +31,11 @@ for (x in list){
 #trees ploting
 
 Cassidinae_28S_D2_PhyML_JC69 <- read.tree("trees/Cassidinae_28S_D2_PhyML_JC69.tre")
-plot.phylo(Cassidinae_28S_D2_PhyML_JC69, "t", cex = 0.9, no.margin = TRUE )
+plot.phylo(Cassidinae_28S_D2_PhyML_JC69, "t", cex = 0.9, no.margin = TRUE, use.edge.length=FALSE , show.node.label = T)
 nodelabels(adj = -0.2, frame = "n", cex = 0.8, font = 2)
 
 Cassidinae_28S_D2_PhyML_JC69 <- read.tree("trees/Cassidinae_28S_D2_PhyML_GTR")
-plot.phylo(Cassidinae_28S_D2_PhyML_JC69, "t", cex = 0.9, no.margin = TRUE )
+plot.phylo(Cassidinae_28S_D2_PhyML_JC69, "t", cex = 0.9, no.margin = TRUE, use.edge.length=FALSE , show.node.label = T)
 nodelabels(adj = -0.2, frame = "n", cex = 0.8, font = 2)
 
 Eulo_28S_D2_PhyML_JC69 <- read.tree("trees/Eulo_28S_D2_PhyML_JC69.tre")
@@ -45,9 +45,13 @@ Eulo_ITS2_PhyML_JC69 <- read.tree("trees/Eulo_ITS2_PhyML_JC69.tre")
 plot.phylo(Eulo_ITS2_PhyML_JC69, show.node.label = TRUE)
 
 Eulo_Cyt_b_PhyML_JC69 <- read.tree("trees/Eulo_Cyt_b_PhyML_JC69.tre")
-plot.phylo(Eulo_Cyt_b_PhyML_JC69, "t", cex = 0.9, no.margin = TRUE)
+plot.phylo(Eulo_Cyt_b_PhyML_JC69, "t", cex = 0.9, no.margin = TRUE, use.edge.length=FALSE)
 nodelabels(adj = -0.2, frame = "n", cex = 0.8, font = 2)
 
 Eulo_Cyt_b_PhyML_JC69 <- read.tree("trees/Eulo_Cyt_b_PhyML_GTR")
 plot.phylo(Eulo_Cyt_b_PhyML_JC69, "t", cex = 0.9, no.margin = TRUE)
+nodelabels(adj = -0.2, frame = "n", cex = 0.8, font = 2)
+
+Eulo_Cyt_b_PhyML_JC69 <- read.tree("trees/Eulo_Cyt_b_PhyML_GTR_modified")
+plot.phylo(Eulo_Cyt_b_PhyML_JC69, "t", cex = 0.9, no.margin = TRUE, use.edge.length=FALSE, show.node.label = T)
 nodelabels(adj = -0.2, frame = "n", cex = 0.8, font = 2)
